c1ed2ae38a1e4d150c1a43c0622a6f0a866f6b43
florisvde <floris.vanderent@gmail.com>
2023-05-31 14:53:23 +0200

    fixed stick not turning off on Raspberry Pi
    Closing the port directly after sending the byte did not clock in RTS properly on a Raspberry Pi. Therefore added a delay before closing.

--- Python/stick_off.py
+++ Python/stick_off.py
@@ -1,4 +1,6 @@
 import serial
+import time
+
 #ser = serial.Serial('/dev/ttyUSB0') # open serial port (UNIX)
 ser = serial.Serial('COM5') # open serial port (Windows)
 
@@ -8,5 +10,7 @@ ser = serial.Serial('COM5') # open serial port (Windows)
 ser.setRTS(True) # power off
 
 ser.write(b'\xAA') # send arbitrary data to clock in the RTS state on the flip flop
+
+time.sleep(0.1) # neccesarry on Unix, otherwise RTS is not clocked in properly
 
 ser.close()
